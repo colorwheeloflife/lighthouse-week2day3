57ca406ed01909291aad260778a1a75a4e98f575
Taylor Aucoin <colorwheeloflife@gmail.com>
2016-09-17 00:17:57 +0000

    Update day 2 completed versions

--- express_server.js
+++ express_server.js
@@ -1,13 +1,27 @@
 var express = require("express");
 var app = express();
+
+var methodOverride = require("method-override");
+app.use(methodOverride('_method'));
+
 app.set("view engine", "ejs");
-var fs = require("fs");
 var PORT = process.env.PORT || 3000; // default port 8080
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded());
 
 app.use(express.static('public'));
+
+
+// var connect = require('connect');
+
+
+
+
+
+//
+
+
 
 
 function generateRandomString() {
@@ -21,62 +35,88 @@ function generateRandomString() {
 }
 
 
-// var urlDatabases = req.body.longURL;
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-// var length = Object.keys(urlDatabase).length - 1;
-// var yourURL = Object.keys(urlDatabase)[length];
 
-
-
-
-
-
+// TO INDEX
 app.get("/urls", (req, res) => {
   var templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+// // TO NEW
+// app.get("/urls/new", (req, res) => {
+//   res.render("urls_new");
+// });
+
+
+// // FROM NEW — TO CREATE
+// app.post("/urls", (req, res) => {
+//   var theShortURL = generateRandomString();
+//   var theLongURL = req.body.longURL;
+//   urlDatabase[theShortURL] = theLongURL;
+//   res.redirect('/urls/shortURL');
+// });
+
+
+// FROM NEW — TO DELETE
+app.delete("/urls/:sURL", (req, res) => {
+  var daKeyToRemove = req.params.sURL;
+  delete urlDatabase[daKeyToRemove];
+  res.redirect('/urls');
 });
 
 
-app.post("/urls", (req, res) => {
-  // res.send(req.body.longURL);
-  var theShortURL = generateRandomString();
-  var theLongURL = req.body.longURL;
-  urlDatabase[theShortURL] = theLongURL;
-  res.redirect('/urls/shortURL');
-   // debug statement to see POST parameters
-   // Respond with 'Ok' (we will replace this)
+// TO SHOW BASED ON ID
+app.get("/urls/:key/edit", (req, res) => {
+  var templateVars = {
+    longURL: urlDatabase[req.params.key],
+    shortURL: req.params.key
+  };
+  res.render("urls_show", templateVars);
 });
 
 
+// TO UPDATE - REDIRECT
+// app.post("/urls/:sURL/edit", (req, res) => {
+//   // var theURLToEdit = req.params.sURL;
+//     var keyToEdit = req.params.sURL;
+//   res.redirect('/urls/:sURL/show');
+// });
 
-app.get("/urls/shortURL", (req, res) => {
-  var length = Object.keys(urlDatabase).length - 1;
-  var theShortenedURL = Object.keys(urlDatabase)[length];
-  res.render("urls_create", {shortURL: theShortenedURL});
+
+// FROM SHOW - TO UPDATE
+app.put("/urls/:shortURL", (req, res) => {
+  console.log(req.params.shortURL + "something");
+  var theKeyToEdit = req.params.shortURL;
+  var editedValue = req.body.editedURL;
+  urlDatabase[theKeyToEdit] = editedValue;
+  res.redirect('/urls');
 });
 
 
-// for loop??
-app.get("/u/:shortURL", (req, res) => {
-  var longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
-});
+// // TO SHORTURL PAGE
+// app.get("/urls/shortURL", (req, res) => {
+//   var length = Object.keys(urlDatabase).length - 1;
+//   var theShortenedURL = Object.keys(urlDatabase)[length];
+//   res.render("urls_create", {shortURL: theShortenedURL});
+// });
+
+
+// // TO ACTUAL WEBLINK
+// app.get("/u/:shortURL", (req, res) => {
+//   var longURL = urlDatabase[req.params.shortURL];
+//   res.redirect(longURL);
+// });
 
 
 
-app.get("/hello", (req, res) => {
-  res.end("<html><body>Hello <b>World</b></body></html>\n");
-});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
